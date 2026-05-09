70e80c72608f913fd0d813a422e18fd5719ecb27
chihchunhsiao <chsiao@byupathway.edu>
2026-05-09 11:26:24 +0800

    week1 code hw

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,18 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // 1. Initialize a result array with the the specified length.
+        double[] result = new double[length];
+
+        // 2. Use a "for" loop to iterate through the "result" array.
+        for (int i = 0; i < length; i++)
+        {
+            // 3. Calculate the multiple and store it in the result array.
+            result[i] = number * (i + 1);           
+        }
+
+        // 4. Return the result array.
+        return result; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +40,24 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // 1. Set a newList with the same capacity as the original data.
+        List<int> newList = new List<int>(data.Count);
+        // 2. Determine the starting index for the rotation.
+        int currentIndex = data.Count - amount;
+
+        // 3. Use a "for" loop to iterate through the original data.
+        for (int i = 0; i < data.Count; i++)
+        {
+            // 4. Use the modulo operator (%) to take the remainder as the index of the original data, and obtain the new rotated list value.
+            int value = data[(i + currentIndex) % data.Count];  
+
+            // 5. Add the value to the newList.
+            newList.Add(value);
+        }
+
+        // 6. Update the original data by clearing it and adding all elements from newList.
+        data.Clear();
+        data.AddRange(newList);
     }
 }
